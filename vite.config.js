2ac737f36e8a5ef77837814bbbfb6fefc00ad158
Sobitmv <sobit.sharma@mvmobility.in>
2024-03-27 18:10:34 -0400

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/Staff_Attendance_Management_system/",
   plugins: [react()],
-  base:"Staff_Attendance_Management_system"
 })
